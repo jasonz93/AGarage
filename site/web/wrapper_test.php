8095210f68e51e97a81dc754d7ecdacc1eea378a
nicholas <nicholas@hrsoft.net>
2016-02-21 06:40:59 +0800

    SAE Compat

--- site/web/wrapper_test.php
+++ site/web/wrapper_test.php
@@ -8,6 +8,8 @@
 
 require_once __DIR__ . '/../app/wrapper.php';
 
-var_dump(@file_put_contents('mysaemc:///var/cache/saedev/classes.php', 'test content') && @rename('mysaemc:///var/cache/saedev/classes.php', 'mysaemc:///var/cache/saedev/classes.php1'));
+//var_dump(@file_put_contents('mysaemc:///var/cache/saedev/classes.php', 'test content') && @rename('mysaemc:///var/cache/saedev/classes.php', 'mysaemc:///var/cache/saedev/classes.php1'));
 
 //var_dump(@rename('mysaemc://test.txt', 'mysaemc://test1.txt'));
+
+var_dump(@file_put_contents(tempnam('aaa', 'bbb'), 'test'));
