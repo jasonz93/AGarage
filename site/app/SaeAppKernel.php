677ff4205da8cf2e7327191da51ef285a0fd8d6a
nicholas <nicholas@hrsoft.net>
2016-02-21 07:53:40 +0800

    SAE Compat

--- site/app/SaeAppKernel.php
+++ site/app/SaeAppKernel.php
@@ -18,7 +18,7 @@ class SaeAppKernel extends AppKernel
 {
     public function getCacheDir()
     {
-        return 'mysaekv:///var/cache/'.$this->getEnvironment();
+        return 'saekv:///var/cache/'.$this->getEnvironment();
     }
 
     /**
