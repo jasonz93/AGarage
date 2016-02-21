b141921689993e0f9440e84d3a74d286f139743c
nicholas <nicholas@hrsoft.net>
2016-02-21 11:34:15 +0800

    SAE Compat

---
+++ site/web/clear.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicholas
+ * Date: 16-2-21
+ * Time: 上午11:32
+ */
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Debug\Debug;
+
+$loader = require __DIR__.'/../app/autoload.php';
+
+$input = new ArgvInput('console assetic:dump');
+$env = $input->getParameterOption(['--env', '-e'], getenv('SYMFONY_ENV') ?: 'dev');
+$debug = getenv('SYMFONY_DEBUG') !== '0' && !$input->hasParameterOption(['--no-debug', '']) && $env !== 'prod';
+
+if ($debug) {
+    Debug::enable();
+}
+
+$kernel = new AppKernel($env, $debug);
+$application = new Application($kernel);
+$application->run($input);
