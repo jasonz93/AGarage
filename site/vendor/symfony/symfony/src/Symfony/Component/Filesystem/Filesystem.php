33ea5fdd0cb44d771119dae0c9b0fc0e7c7878c5
nicholas <nicholas@hrsoft.net>
2016-02-21 11:56:47 +0800

    SAE Compat

--- site/vendor/symfony/symfony/src/Symfony/Component/Filesystem/Filesystem.php
+++ site/vendor/symfony/symfony/src/Symfony/Component/Filesystem/Filesystem.php
@@ -532,7 +532,6 @@ class Filesystem
 
         // Ignore for filesystems that do not support umask
         @chmod($tmpFile, 0666);
-        sae_debug($tmpFile);
         $this->rename($tmpFile, $filename, true);
     }
 
