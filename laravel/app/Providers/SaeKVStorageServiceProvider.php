9291c38130829ade6e09475014cb31a711294e32
nicholas <nicholas@hrsoft.net>
2016-02-21 18:09:48 +0800

    Laravel SAE Compat

---
+++ laravel/app/Providers/SaeKVStorageServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicholas
+ * Date: 16-2-21
+ * Time: 下午6:02
+ */
+
+namespace AGarage\Providers;
+
+use AGarage\Extensions\SaeKVStorageAdapter;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use Storage;
+
+
+class SaeKVStorageServiceProvider extends ServiceProvider
+{
+    public function boot() {
+        Storage::extend('saekv', function ($app, $config) {
+            return new Filesystem(new SaeKVStorageAdapter());
+        });
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
