38867f03c20f6d8c141cbd487e5874f4f228b637
nicholas <nicholas@hrsoft.net>
2016-02-25 00:36:51 +0800

    Resume

--- laravel/vendor/composer/autoload_classmap.php
+++ laravel/vendor/composer/autoload_classmap.php
@@ -12,6 +12,7 @@ return array(
     'CreateBlogCommentsTable' => $baseDir . '/database/migrations/2016_02_22_134332_create_blog_comments_table.php',
     'CreateBlogTopicsTable' => $baseDir . '/database/migrations/2016_02_22_133639_create_blog_topics_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
+    'CreateSessionsTable' => $baseDir . '/database/migrations/2016_02_24_163000_create_sessions_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
