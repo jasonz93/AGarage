058e65a75bd10d205b8d2f4bf45b7208f7507619
nicholas <nicholas@hrsoft.net>
2016-03-07 17:16:56 +0800

    完善GithubHook

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,10 @@ var elixir = require('laravel-elixir');
  |
  */
 
+
 elixir(function(mix) {
     mix.styles(['bootstrap.min.css'], 'public/css/base.css');
     mix.scripts(['jquery.min.js', 'bootstrap.min.js'], 'public/js/base.js');
     mix.copy('resources/assets/markdown', 'public/markdown');
+    mix.exec('php artisan migrate');
 });
--- public/github_hook.php
+++ public/github_hook.php
@@ -5,9 +5,10 @@
  * Date: 16-3-7
  * Time: 下午3:26
  */
+const GITHUB_REPO_URL = 'https://github.com/jasonz93/AGarage';
 
 $header = getHeaders();
-$payload = json_decode(file_get_contents('php://input'));
+$payload = json_decode(file_get_contents('php://input'), true);
 
 $log = fopen(__DIR__.'/../storage/logs/github_hook.log', 'a');
 
@@ -16,13 +17,56 @@ $event = $header['x-github-event'];
 switch ($event) {
     case 'ping':
         writeLog($log, 'Got a ping.');
-        break;
+        goto END;
+    case 'push':
+        writeLog($log, 'Got a push.');
+        goto HANDLE_PUSH_EVENT;
     default:
         writeLog($log, "Got an unknown event: $event .");
-        break;
+        writeLog($log, json_encode($payload, JSON_PRETTY_PRINT));
+        goto END;
 }
 
+HANDLE_PUSH_EVENT:
+$projectPath = dirname(__DIR__.'/../');
+chdir($projectPath);
+$branch = explode('/', $payload['ref'])[2];
+writeLog($log, "Push Branch: $branch");
+$output = [];
+exec('git status', $output);
+$currentBranch = explode(' ', $output[0])[1];
+writeLog($log, "Current Branch: $currentBranch");
+$headCommit = $payload['head_commit'];
+$repository = $payload['repository'];
+if ($repository['url'] !== GITHUB_REPO_URL) {
+    writeLog($log, "Not this repository.");
+    goto END;
+}
+if ($currentBranch !== $branch) {
+    writeLog($log, "Not current branch.");
+    goto END;
+}
+if (count($headCommit['added']) + count($headCommit['removed']) + count($headCommit['modified']) == 0) {
+    writeLog($log, "No file changed.");
+    goto END;
+}
+$output = [];
+writeLog($log, "Updating project from github...");
+exec("git pull", $output);
+writeLog($log, $output);
+$output = [];
+writeLog($log, "Running gulp...");
+exec("gulp", $output);
+writeLog($log, $output);
+
+goto END;
+
+
+END:
+writeLog($log, "Github Hook done.");
 fclose($log);
+
+
 
 /**
  * 获取HTTP头
@@ -42,9 +86,15 @@ function getHeaders() {
 }
 
 function writeLog($resource, $content) {
-    $lines = explode("\n", $content);
+    if (is_string($content)) {
+        $lines = explode("\n", $content);
+    } else if (is_array($content)) {
+        $lines = $content;
+    } else {
+        return;
+    }
     $date = date('Y-m-d H:i:s');
     foreach ($lines as $line) {
         fprintf($resource, "%s --- %s\n", $date, $line);
     }
-}
+};
